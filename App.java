5298b1c1f1a8989690ff19325483c5242fd23a13
pvfcosta <pedrovfc11@gmail.com>
2023-03-17 18:59:05 -0300

    Criação do código java metricas antes da atualização e adição de linhas de código

---
+++ App.java
@@ -0,0 +1,15 @@
+//comentário teste
+
+//comentário teste
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Pedro Vítor Felix da Costa");
+        System.out.println("Engenharia Econômica - PUC Minas");
+
+
+
+
+
+
+    }
+}
